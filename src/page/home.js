7ead86e4c06eed722863e2f6efb3a73a56b6709d
XphantomX <geguna27@gmail.com>
2023-01-09 03:01:37 +0400

    Index changes

--- src/page/home.js
+++ src/page/home.js
@@ -13,10 +13,386 @@ const [cz, Setcz] = useState({aba:'15',baba:'25'})
   
 <Main>
 
-<div class="col-9 rnmg24">
+<div class="col-9 rnmg24_nw">
 
 
-<div class="nlnch2">Right now at online: <a href="#">1252</a></div>
+
+<div class="row" style={{marginTop: '10px'}}>
+	
+	
+
+
+<div class="col-12">
+
+
+<div class="c123czt13afqfz_22">Randomly chosen users</div>
+
+
+<div class="row">
+
+<div class="col-6">
+
+<div class="c123czt13afqf">
+
+<div class="TiTle">
+Girls You may know 
+</div>
+
+
+
+<div class="c13zzzz_all_items cqwezzz22itmnlst">
+
+<div><img src="pics/CYjP_-UxpPs.jpg"/></div>
+<div><img src="pics/CYjP_-UxpPs.jpg"/></div>
+<div><img src="pics/CYjP_-UxpPs.jpg"/></div>
+<div><img src="pics/CYjP_-UxpPs.jpg"/></div>
+
+
+
+</div>
+
+<div class="rnmg31">
+<a href="" class="rnmg32">See all</a>
+</div>
+
+</div>
+	
+
+</div>
+
+
+
+<div class="col-6">
+
+<div class="c123czt13afqf">
+
+<div class="TiTle">
+Boys You may know 
+</div>
+
+
+
+<div class="c13zzzz_all_items cqwezzz22itmnlst">
+
+<div><img src="pics/CYjP_-UxpPs.jpg"/></div>
+<div><img src="pics/CYjP_-UxpPs.jpg"/></div>
+<div><img src="pics/CYjP_-UxpPs.jpg"/></div>
+<div><img src="pics/CYjP_-UxpPs.jpg"/></div>
+
+
+
+
+</div>
+
+<div class="rnmg31">
+<a href="" class="rnmg32">See all</a>
+</div>
+
+</div>
+	
+
+
+</div>
+
+
+</div>	
+	
+	
+	
+	
+
+<div class="col-12">
+
+
+<div class="cz_datq">
+
+
+
+<div class="czlast">
+
+<div class="left cz_pdng21">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg"/>
+</div>
+
+<div class="cz_datq_pdngaz">
+<div>qwecqwecqweqwecqwecqwe qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe     </div>
+</div>
+
+<div class="left cz_datq_pdngazz">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg" width="25" height="25"/>	 <a href="">Michell</a>  
+</div>
+
+
+<div class="left cz_datq_pdngazz mnlstgnd cz_2z">
+ 2023-01-11
+</div>
+
+<div class="right" style={{paddingRight:'10px'}} id="rght_details">
+	
+<span><svg class="cp_share_11"></svg> 15</span>	
+	
+	
+<span>
+<svg class="cmnts_otqz22"></svg> 125	
+	</span>
+	
+
+<span>
+<svg class="lzzzf123a_22"></svg> 15 </span>
+
+
+
+</div>
+
+
+</div>
+
+
+<div class="czlast">
+
+<div class="left cz_pdng21">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg"/>
+</div>
+
+<div class="cz_datq_pdngaz">
+<div>qwecqwecqweqwecqwecqwe qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe     </div>
+</div>
+
+<div class="left cz_datq_pdngazz">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg" width="25" height="25"/>	 <a href="">Michell</a>  
+</div>
+
+
+<div class="left cz_datq_pdngazz mnlstgnd cz_2z">
+ 2023-01-11
+</div>
+
+<div class="right" style={{paddingRight:'10px'}} id="rght_details">
+	
+<span><svg class="cp_share_11"></svg> 15</span>	
+	
+	
+<span>
+<svg class="cmnts_otqz22"></svg> 125	
+	</span>
+	
+
+<span>
+<svg class="lzzzf123a_22"></svg> 15 </span>
+
+
+
+</div>
+
+
+</div>
+
+
+<div class="czlast">
+
+<div class="left cz_pdng21">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg"/>
+</div>
+
+<div class="cz_datq_pdngaz">
+<div>qwecqwecqweqwecqwecqwe qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe     </div>
+</div>
+
+<div class="left cz_datq_pdngazz">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg" width="25" height="25"/>	 <a href="">Michell</a>  
+</div>
+
+
+<div class="left cz_datq_pdngazz mnlstgnd cz_2z">
+ 2023-01-11
+</div>
+
+<div class="right" style={{paddingRight:'10px'}} id="rght_details">
+	
+<span><svg class="cp_share_11"></svg> 15</span>	
+	
+	
+<span>
+<svg class="cmnts_otqz22"></svg> 125	
+	</span>
+	
+
+<span>
+<svg class="lzzzf123a_22"></svg> 15 </span>
+
+
+
+</div>
+
+
+</div>
+
+
+
+<div class="czlast">
+
+<div class="left cz_pdng21">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg"/>
+</div>
+
+<div class="cz_datq_pdngaz">
+<div>qwecqwecqweqwecqwecqwe qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe     </div>
+</div>
+
+<div class="left cz_datq_pdngazz">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg" width="25" height="25"/>	 <a href="">Michell</a>  
+</div>
+
+
+<div class="left cz_datq_pdngazz mnlstgnd cz_2z">
+ 2023-01-11
+</div>
+
+<div class="right" style={{paddingRight:'10px'}} id="rght_details">
+	
+<span><svg class="cp_share_11"></svg> 15</span>	
+	
+	
+<span>
+<svg class="cmnts_otqz22"></svg> 125	
+	</span>
+	
+
+<span>
+<svg class="lzzzf123a_22"></svg> 15 </span>
+
+
+
+</div>
+
+
+</div>
+
+
+
+<div class="czlast">
+
+<div class="left cz_pdng21">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg"/>
+</div>
+
+<div class="cz_datq_pdngaz">
+<div>qwecqwecqweqwecqwecqwe qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe     </div>
+</div>
+
+<div class="left cz_datq_pdngazz">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg" width="25" height="25"/>	 <a href="">Michell</a>  
+</div>
+
+
+<div class="left cz_datq_pdngazz mnlstgnd cz_2z">
+ 2023-01-11
+</div>
+
+<div class="right" style={{paddingRight:'10px'}} id="rght_details">
+	
+<span><svg class="cp_share_11"></svg> 15</span>	
+	
+	
+<span>
+<svg class="cmnts_otqz22"></svg> 125	
+	</span>
+	
+
+<span>
+<svg class="lzzzf123a_22"></svg> 15 </span>
+
+
+
+</div>
+
+
+</div>
+
+
+<div class="czlast">
+
+<div class="left cz_pdng21">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg"/>
+</div>
+
+<div class="cz_datq_pdngaz">
+<div>qwecqwecqweqwecqwecqwe qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe  qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe    qwecqwecqwe     </div>
+</div>
+
+<div class="left cz_datq_pdngazz">
+<img src="pics/d281cf9550d6abc5e3824f6b55e57983.jpg" width="25" height="25"/>	 <a href="">Michell</a>  
+</div>
+
+
+<div class="left cz_datq_pdngazz mnlstgnd cz_2z">
+ 2023-01-11
+</div>
+
+<div class="right" style={{paddingRight:'10px'}} id="rght_details">
+	
+<span><svg class="cp_share_11"></svg> 15</span>	
+	
+	
+<span>
+<svg class="cmnts_otqz22"></svg> 125	
+	</span>
+	
+
+<span>
+<svg class="lzzzf123a_22"></svg> 15 </span>
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+	
+</div>
+		
+	
+	
+	
+
+</div>
+	
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+
+		</div>
+
+
 
 </div>
 
