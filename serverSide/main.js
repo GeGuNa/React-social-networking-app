e6e8550d48686c4cb5c7711edecd72996f61c805
Phantom <abracadabra1253@gmail.com>
2023-12-30 18:59:11 +0400

    Chatrooms

--- serverSide/main.js
+++ serverSide/main.js
@@ -10,6 +10,23 @@ res.end()
 })
 
 
+app.post("/login", (req, res) => {
+	
+res.write(``)
+
+res.end()
+})
+
+
+app.post("/register", (req, res) => {
+	
+res.write(``)
+
+res.end()
+})
+
+
+
 app.listen(3333, () => {
 	
 	console.log(`yeah`)
